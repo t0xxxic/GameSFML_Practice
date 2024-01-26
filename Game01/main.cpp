c83a281fcbead5e05a78fa046b17ee896a35431b
Sam Joel <30547327+ssamjoel@users.noreply.github.com>
2024-01-26 20:27:03 +0530

    initial Commit

---
+++ Game01/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+int main()
+{
+    std::cout << "SFML Works!" << std::endl;
+    return 0;
+}
